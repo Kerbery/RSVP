ecbc4a20e6c49d39067c9b42c86d37486b9f5336
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-01 13:13:40 +0200

    Add project files.

---
+++ RSVP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RSVP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
