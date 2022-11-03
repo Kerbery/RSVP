d8729b1c7fe9347fe91ca05d2b0748d4d343fdb3
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-03 13:30:22 +0200

    Added client-side validation for attendence radio buttons

--- RSVP/Models/UserViewModel.cs
+++ RSVP/Models/UserViewModel.cs
@@ -10,6 +10,9 @@ namespace RSVP.Models
 
         [StringLength(20, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
         public string Surname { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select an option to indicate attendence.")]
+        [EnumDataType(typeof(Attendence), ErrorMessage = "Attendence value doesn't exist in enum.")]
         public Attendence Attendence { get; set; } = Attendence.NotSure;
     }
 }
